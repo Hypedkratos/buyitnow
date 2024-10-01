7e44f1f3159a677dc05a66dbdd78277ea0f5559a
Hypedkratos <shashianand8651@gmail.com>
2024-10-01 08:47:57 +0530

    modified variables.scss file

---
+++ src/pages/HomeScreen.tsx
@@ -0,0 +1,20 @@
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./HomeScreen.scss";
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonContent>
+        <div>smdjcndnvnnfven</div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
